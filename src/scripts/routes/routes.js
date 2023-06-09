51611d3920304c964ebd853d084f98fb04bf814b
npannisa <nurulprimaa@gmail.com>
2023-06-09 17:31:16 +0700

    add contactus-pages

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -1,10 +1,12 @@
 import Restaurant from '../views/pages/restaurant-pages';
 import Detail from '../views/pages/detail-pages';
 import Favorite from '../views/pages/favorite-pages';
+import ContactUs from '../views/pages/contactus-pages';
 
 const routes = {
   '/': Restaurant,
   '/favorite': Favorite,
   '/detail/:id': Detail,
+  '/#/contact-us' : ContactUs,
 };
 export default routes;
