f28eb9d341282947593cedc149262dbf8c78a14d
iiizal15 <fahrizal.20113@mhs.unesa.ac.id>
2023-06-15 22:38:41 +0700

    Update Folder dan Fitur

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import DetailPatriot from '../view/pages/detail-patriot';
 import Home from '../view/pages/home';
 import Patriot from '../view/pages/patriot';
@@ -9,18 +8,6 @@ const routes = {
   '/patriot': Patriot,
   '/detail-patriot/:id': DetailPatriot,
   '/contactus': Contactus,
-=======
-import Restaurant from '../views/pages/restaurant-pages';
-import Detail from '../views/pages/detail-pages';
-import Favorite from '../views/pages/favorite-pages';
-import ContactUs from '../views/pages/contactus-pages';
-
-const routes = {
-  '/': Restaurant,
-  '/favorite': Favorite,
-  '/detail/:id': Detail,
-  '/#/contact-us' : ContactUs,
->>>>>>> 338b5e43ae095ee48da6e0a778f3a5ed221fe3b5
 };
 
 export default routes;
