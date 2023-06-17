137d1fdc0665798e86c42eff9f24b63bac960489
zidaann <jourjizaidan8@gmail.com>
2023-06-17 20:10:40 +0700

    add responsive home

--- src/scripts/view/pages/home.js
+++ src/scripts/view/pages/home.js
@@ -12,49 +12,48 @@ const Home = {
         </div>
 
         <!-- Article Section -->
-        <article class="article">
-          <div class="article1">
-            <div class="article1__desc">
-              <h3>Pahlawan mengajarkan bagaimana cara menghargai arti dari perjuangan</h3>
+        <div class="my-3">
+          <article class="article">
+            <div class="article1">
+              <div class="article1__desc">
+                <h3>Pahlawan mengajarkan bagaimana cara menghargai arti dari perjuangan</h3>
+              </div>
+              <div class="article1__image">
+                <img src="./img/hero2.png" alt="Gambar Article 1" class="lazyload" />
+              </div>
             </div>
-            <div class="article1__image">
-              <img src="./img/hero2.png" alt="Gambar Article 1" class="lazyload" />
+          </article>
+          <article class="articles">
+            <div class="article2">
+              <div class="article2__image">
+                <img src="./img/hero3.png" alt="Gambar Article 2" class="lazyload" />
+              </div>
+              <div class="article2__desc">
+                <h3>Teladan Bagi Masyarakat dalam memperjuangkan bangsa Indonesia</h3>
+              </div>
             </div>
-          </div>
-        </article>
-
-        <article class="articles">
-          <div class="article2">
-            <div class="article2__image">
-              <img src="./img/hero3.png" alt="Gambar Article 2" class="lazyload" />
+          </article>
+          <article class="articlet">
+            <div class="article3">
+              <div class="article3__desc">
+                <h3>Memaknai arti persatuan dalam sebuah negara</h3>
+              </div>
+              <div class="article3__image">
+                <img src="./img/hero4.png" alt="Gambar Article 3" class="lazyload" />
+              </div>
             </div>
-            <div class="article2__desc">
-              <h3>Teladan Bagi Masyarakat dalam memperjuangkan bangsa Indonesia</h3>
+          </article>
+          <article class="articlex">
+            <div class="article4">
+              <div class="article4__image">
+                <img src="./img/hero5.png" alt="Gambar Article 4" class="lazyload" />
+              </div>
+              <div class="article4__desc">
+                <h3>Lebih mengenal sejarah bangsa yang beragam</h3>
+              </div>
             </div>
-          </div>
-        </article>
-
-        <article class="articlet">
-          <div class="article3">
-            <div class="article3__desc">
-              <h3>Memaknai arti persatuan dalam sebuah negara</h3>
-            </div>
-            <div class="article3__image">
-              <img src="./img/hero4.png" alt="Gambar Article 3" class="lazyload" />
-            </div>
-          </div>
-        </article>
-
-        <article class="articlex">
-          <div class="article4">
-            <div class="article4__image">
-              <img src="./img/hero5.png" alt="Gambar Article 4" class="lazyload" />
-            </div>
-            <div class="article4__desc">
-              <h3>Lebih mengenal sejarah bangsa yang beragam</h3>
-            </div>
-          </div>
-        </article>
+          </article>
+        </div>
         `;
   },
 
